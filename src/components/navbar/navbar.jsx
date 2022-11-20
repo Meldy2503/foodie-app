6f3e036b4e5c3e28f2b6eb7e3de28dacd0153a8e
meldy2503 <emelder.charles25@gmail.com>
2022-11-20 16:21:44 +0100

    fix: modify navbar style

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Sling as Hamburger } from "hamburger-react";
-import { ClickAwayListener } from "@material-ui/core";
 import LogoImg from "../../assets/images/foodie-logo.svg";
 
 import {
@@ -21,69 +20,66 @@ const Navbar = ({ bgCo, position }) => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const handleMenuIcon = () => setIsMenuOpen(!isMenuOpen);
   const closeMenuLink = () => setIsMenuOpen(false);
-  const handleClickAway = () => setIsMenuOpen(false);
 
   return (
-    <ClickAwayListener onClickAway={handleClickAway}>
-      <Header bgCo={bgCo} position={position}>
-        <NavContainer>
-          <Logo>
-            <Link to="/">
-              <img src={LogoImg} alt="foodie-app logo" />
-              <h2>foodie</h2>
-            </Link>
-          </Logo>
-          <LinkContainer isMenuOpen={isMenuOpen} bgCo={bgCo}>
-            <Wrapper>
-              <LinkLists>
-                <Link to="/about-us" onClick={closeMenuLink}>
-                  About Us
-                </Link>
-              </LinkLists>
-              <LinkLists>
-                <Medium
-                  href="https://medium.com/foodienig"
-                  target="_blank"
-                  onClick={closeMenuLink}
-                >
-                  Learn
-                </Medium>
-              </LinkLists>
-            </Wrapper>
-            <Wrapper>
-              <LinkLists>
-                <Link to="/pricing" onClick={closeMenuLink}>
-                  Pricing
-                </Link>
-              </LinkLists>
-              <LinkLists>
-                <Link to="/contact-us" onClick={closeMenuLink}>
-                  Contact Us
-                </Link>
-              </LinkLists>
-              <LinkLists>
-                <Link to="/create-account" onClick={closeMenuLink}>
-                  Login
-                </Link>
-              </LinkLists>
+    <Header bgCo={bgCo} position={position}>
+      <NavContainer>
+        <Logo>
+          <Link to="/">
+            <img src={LogoImg} alt="foodie-app logo" />
+            <h2>foodie</h2>
+          </Link>
+        </Logo>
+        <LinkContainer isMenuOpen={isMenuOpen} bgCo={bgCo}>
+          <Wrapper>
+            <LinkLists>
+              <Link to="/about-us" onClick={closeMenuLink}>
+                About Us
+              </Link>
+            </LinkLists>
+            <LinkLists>
+              <Medium
+                href="https://medium.com/foodienig"
+                target="_blank"
+                onClick={closeMenuLink}
+              >
+                Learn
+              </Medium>
+            </LinkLists>
+          </Wrapper>
+          <Wrapper>
+            <LinkLists>
+              <Link to="/pricing" onClick={closeMenuLink}>
+                Pricing
+              </Link>
+            </LinkLists>
+            <LinkLists>
+              <Link to="/contact-us" onClick={closeMenuLink}>
+                Contact Us
+              </Link>
+            </LinkLists>
+            <LinkLists>
+              <Link to="/create-account" onClick={closeMenuLink}>
+                Login
+              </Link>
+            </LinkLists>
 
-              <GetStarted>
-                <Link2 to="/create-account">Get Started</Link2>
-              </GetStarted>
-            </Wrapper>
-          </LinkContainer>
-          <MenuIcon onClick={handleMenuIcon}>
-            <Hamburger
-              rounded
-              color="#000"
-              size={25}
-              toggled={isMenuOpen}
-              toggle={setIsMenuOpen}
-            />
-          </MenuIcon>
-        </NavContainer>
-      </Header>
-    </ClickAwayListener>
+            <GetStarted>
+              <Link2 to="/create-account">Get Started</Link2>
+            </GetStarted>
+          </Wrapper>
+        </LinkContainer>
+        <MenuIcon onClick={handleMenuIcon}>
+          <Hamburger
+            rounded
+            color="#000"
+            size={25}
+            toggled={isMenuOpen}
+            toggle={setIsMenuOpen}
+          />
+        </MenuIcon>
+      </NavContainer>
+    </Header>
   );
 };
 
